a1a92e0c31b579205636d055c1c5800be1129b65
Aakash <21053242@kiit.ac.in>
2022-04-07 11:52:19 +0530

    Adding Files

--- college code/pyramid.c
+++ college code/pyramid.c
@@ -5,18 +5,22 @@ int main()
     int n;
     char c = '$';
     scanf("%d", &n);
+    int count1 = n - 1, count2 = n - 1, count3 = 3 * n - 2, count4 = 3 * n - 2;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - i; j++)
+        for (int j = 0; j < 4 * n - 2; j++)
         {
-            printf(" ");
+            if ((j >= count1 && j <= count2) || (j >= count3 && j <= count4))
+                printf("%c", c);
+            else
+                printf(" ");
         }
-        for (int j = 0; j < n; j++)
-        {
-            if (i > j || i == j)
-                printf("%c ", c);
-        }
-        printf("\n");
+        printf(" \n");
+        count1--;
+        count2++;
+        count3--;
+        count4++;
     }
+
     return 0;
 }
--- college code/pyramidclass.cpp
+++ college code/pyramidclass.cpp
@@ -4,12 +4,20 @@ int main()
 {
     int n;
     cin >> n;
+    int count1 = n - 1, count2 = n - 1;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < 2 * n - 1; j++)
         {
-            cout << i << j << " ";
+            if (j >= count1 && j <= count2)
+
+                cout << i << j << " ";
+
+            else
+                printf("   ");
         }
+        count1--;
+        count2++;
         cout << endl;
     }
     return 0;
